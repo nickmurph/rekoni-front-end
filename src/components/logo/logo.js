6b9325652790258130a5fcce65552665b26cd999
nickmurph <nfmurphy@buffalo.edu>
2023-02-14 01:02:06 -0700

    tweak to padding style of logo's tilt-inner class

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -7,9 +7,9 @@ import HighResLogo from './logo-black-highres-transparent.png';
 const Logo = ()  => {
     return (
         <div className="ma4 mt0" style={{paddingLeft: "3.5%"}}>
-            <Tilt classname="Tilt br2 shadow-2" tiltMaxAngleX={10} tiltMaxAngleY={3} tiltReverse={true}>
+            <Tilt classname="Tilt br2 shadow-2" tiltMaxAngleX={10} tiltMaxAngleY={3} tiltReverse={false}>
                 <div style={{ height: '14%', width: '10%', background: "transparent"}}>
-                    <h1 className="Tilt-inner"><img alt='logo' src={HighResLogo}/></h1>
+                    <h1 className="Tilt-inner pa3"><img style={{paddingTop: '5px'}}alt='logo' src={HighResLogo}/></h1>
                 </div>
             </Tilt>
         </div>

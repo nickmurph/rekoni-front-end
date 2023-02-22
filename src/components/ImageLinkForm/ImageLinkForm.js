288d5b34850c14a65d88ff3310aa0079a0c1b75f
nickmurph <nfmurphy@buffalo.edu>
2023-02-21 23:27:46 -0700

    added ImageLinkForm css

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -7,9 +7,9 @@ const ImageLinkForm = ()  => {
                 {'Rekoni will detect faces in your pictures. Give it a try!'}
             </p>
             <div className='center'>
-                <div classname="pa4 br3 shadow-5">
-                <input className ='f4 pa2 w-70 center' type='text' />
-                <button className='w-30 grow f4 link ph3 pv dib white bg-light-purple center'>Detect</button>
+                <div classname="center pa4 br3 shadow-5 center">
+                    <input className ='f4 pa2 w-70 center' type='text' />
+                    <button className='w-30 grow f4 link ph3 pv dib white bg-light-purple center'>Detect</button>
                 </div>
             </div>
         </div>

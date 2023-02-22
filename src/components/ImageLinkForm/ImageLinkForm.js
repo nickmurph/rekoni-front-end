236314f8b4cde157d6e5f4e29328b1ca0fb06489
nickmurph <nfmurphy@buffalo.edu>
2023-02-22 01:12:58 -0700

    More UI tweaks, departing from ZTM design

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -10,7 +10,7 @@ const ImageLinkForm = ()  => {
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-1'>
-                    <input className ='f4 pa2 w-70 center' type='text' />
+                    <input className ='f4 pa2 w-70 bg-lightest-blue center' type='text' />
                     <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
                 </div>
             </div>
--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -7,7 +7,7 @@ import HighResLogo from './logo-black-highres-transparent.png';
 const Logo = ()  => {
     return (
         <div className="ma4 mt0" style={{paddingLeft: "3.5%"}}>
-            <Tilt className="Tilt br2 shadow-2" tiltMaxAngleX={20} tiltMaxAngleY={20} tiltReverse={false}  style={{ height: '14%', width: '10%'}}>
+            <Tilt className="Tilt ba br4 shadow-1" tiltMaxAngleX={20} tiltMaxAngleY={20} tiltReverse={false}  style={{ height: '14%', width: '10%', background: ' #cdecff'}}>
                     <h1 className="Tilt-inner pa3"><img style={{paddingTop: '5px'}}alt='logo' src={HighResLogo}/></h1>
             </Tilt>
         </div>

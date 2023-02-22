7b312ffafdad7040afff95e67317c7e2cc813095
nickmurph <nfmurphy@buffalo.edu>
2023-02-22 00:55:09 -0700

    added Particles-BG to background

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank'
-
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg';
 
 function App() {
   return (
@@ -14,7 +14,9 @@ function App() {
       <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition/> */}
+      <ParticlesBg color="#424957" num={150} type="cobweb" bg={true} />
     </div>
+    
   );
 }
 

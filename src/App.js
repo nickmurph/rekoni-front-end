b156da4e31ad2835bbb4e0b8784616cbcbb8defa
nickmurph <nfmurphy@buffalo.edu>
2023-02-26 23:57:20 -0700

    constructor with state, converting from function component to class component to accommodate constructor
    ZTM material uses a class component and constructor within, cant use constructor with functional component. will likely convert back to function component and replace constructor here with hooks later

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
@@ -6,22 +6,31 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg';
 
-function App() {
-  return (
-    <div className="App">
-      <br />
-      <div className="inline-flex flex-row"><Logo /><Navigation /></div>
-      <br />
-      <br />
-      <br />
-      <br />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition/> */}
-      <ParticlesBg color="#424957" num={150} type="cobweb" bg={true} />
-    </div>
-    
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <br />
+        <div className="inline-flex flex-row"><Logo /><Navigation /></div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceRecognition/> */}
+        <ParticlesBg color="#424957" num={150} type="cobweb" bg={true} />
+      </div>   
+    );
+  }
 }
 
 export default App;

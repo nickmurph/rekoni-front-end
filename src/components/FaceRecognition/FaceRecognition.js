0040512b5ead905b85a3cb6d810fe589d6d97121
nickmurph <nfmurphy@buffalo.edu>
2023-03-01 17:38:02 -0700

    minor tweak to display properties of the submitted image

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const FaceRecognition = ({imageURL})  => {
     return (
-        <div className='center'>
-            <img alt='' src={imageURL} />
+        <div className='center ma'>
+            <div className='absolute mt2'>
+            <img width='500px' height='auto' alt='' src={imageURL} />
+            </div>
         </div>
     )
 };

eaaabdd603b7e996dd7306c84aab4ad22e85723e
nickmurph <nfmurphy@buffalo.edu>
2023-03-02 00:03:07 -0700

    Register component and route state integration

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 
 import ParticlesBg from 'particles-bg';
 import {clarifaiKey, clarifaiPAT} from './ClarifaiKey.js';
@@ -106,15 +107,19 @@ class App extends Component {
         <br />
         <br />
         <br />
-        {this.state.route === 'SignIn'
-        ? <SignIn onRouteChange={this.onRouteChange} />
-        : <div>
+        {this.state.route === 'Home'
+        ? <div>
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
         <br />
         <FaceRecognition box={this.state.box} imageURL={this.state.imageURL} />
         </div>
-        }
+        : (
+          this.state.route === 'SignIn' 
+          ? <SignIn onRouteChange={this.onRouteChange} />
+          : <Register onRouteChange={this.onRouteChange} />
+        )
+      }
     
         <ParticlesBg color="#cad0db" num={150} type="cobweb" bg={true} />
       </div>   

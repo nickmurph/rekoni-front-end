7c611d5f088119e598a28b5ca3295cac8a4d04eb
nickmurph <nfmurphy@buffalo.edu>
2023-03-01 22:34:57 -0700

    face outline box now working

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -6,7 +6,7 @@ const FaceRecognition = ({imageURL, box})  => {
         <div className='center ma'>
             <div className='absolute mt2'>
             <img id='inputImage' width='500px' height='auto' alt='' src={imageURL} />
-            <div className="bounding-box"></div>
+            <div className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
             </div>
         </div>
     )

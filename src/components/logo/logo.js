e8da6ff4199f247a12952eb395632cacc89c8517
nickmurph <nfmurphy@buffalo.edu>
2023-03-02 00:43:26 -0700

    fixed invalid DOM property for error (use htmlFor with React JSX), working on nav bar conditional appearance

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -6,7 +6,7 @@ import HighResLogo from './logo-black-highres-transparent.png';
 
 const Logo = ()  => {
     return (
-        <div className="">
+        <div id='logo' className="">
         <div className="ma4 mt0" style={{paddingLeft: "3.5%"}}>
             <Tilt className="Tilt ba br4 shadow-1" tiltMaxAngleX={20} tiltMaxAngleY={20} tiltReverse={false}  style={{ height: 'max(140px,14%)', width: 'max(100px,10%)', background: ' #cdecff'}}>
                     <h1 className="Tilt-inner pa3"><img style={{paddingTop: '5px'}}alt='logo' src={HighResLogo}/></h1>
--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,11 +1,17 @@
 import React from 'react';
+let isSignedIn = false;
 
 const Navigation = ({onRouteChange})  => {
-    return (
+        if (isSignedIn){
+        return (
         <nav className='w-30 pr5' style={{display: 'flex', justifyContent: 'flex-end'}}>
             <p onClick={() => onRouteChange('SignIn')} className="f3 link dim black underline tc pointer">Sign Out</p>
         </nav>
-    )
+        )
+        } else {
+            document.getElementById('logo');
+            return 0;
+        }
 }
 
 export default Navigation;

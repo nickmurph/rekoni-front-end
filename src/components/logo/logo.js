3020e87e097bcefbdbe12b259a495ae7fc4afd16
nickmurph <nfmurphy@buffalo.edu>
2023-03-01 20:24:15 -0700

    using max function to make logo more responsive on smaller screens

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -8,7 +8,7 @@ const Logo = ()  => {
     return (
         <div className="">
         <div className="ma4 mt0" style={{paddingLeft: "3.5%"}}>
-            <Tilt className="Tilt ba br4 shadow-1" tiltMaxAngleX={20} tiltMaxAngleY={20} tiltReverse={false}  style={{ height: '14%', width: '10%', background: ' #cdecff'}}>
+            <Tilt className="Tilt ba br4 shadow-1" tiltMaxAngleX={20} tiltMaxAngleY={20} tiltReverse={false}  style={{ height: 'max(140px,14%)', width: 'max(100px,10%)', background: ' #cdecff'}}>
                     <h1 className="Tilt-inner pa3"><img style={{paddingTop: '5px'}}alt='logo' src={HighResLogo}/></h1>
             </Tilt>
         </div>
